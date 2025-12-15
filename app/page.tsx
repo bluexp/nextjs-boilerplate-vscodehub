7f42a4e484d41f7d223c16ba41b54b433572b879
bluexp <bluexp@126.com>
2025-12-15 15:14:08 +0800

    up

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-4">vscodehub.com</h1>
-      <p className="text-xl text-gray-600">Domain For Sale</p>
-    </div>
-  );
-}
